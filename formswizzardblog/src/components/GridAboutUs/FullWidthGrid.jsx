edd6f3d3610eb94be1efd15e46392fbd76bd1b90
samox_ <samirasteinmayer@web.de>
2023-05-05 17:41:15 +0200

    edited about us section

--- formswizzardblog/src/components/GridAboutUs/FullWidthGrid.jsx
+++ formswizzardblog/src/components/GridAboutUs/FullWidthGrid.jsx
@@ -2,9 +2,11 @@ import Grid from "@mui/material/Unstable_Grid2";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import { Container } from "@mui/material";
 
 //import { Image, Picture } from '@astrojs/image/components';
 
+//display="flex" justifyContent="start" alignItems="start" //rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }} //
 
 //import Avatar4 from "https://i.postimg.cc/x8bxFS7F/Avatar4.png";
 
@@ -16,6 +18,7 @@ const ItemRightAlign = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'start',
+    itemAling: 'start',
     color: theme.palette.text.secondary,
 }));
 
@@ -24,68 +27,77 @@ const ItemLeftAlign = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'end',
+    itemAling: 'end',
     color: theme.palette.text.secondary,
 }));
 
 
 export default function FullWidthGrid() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
 
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }}>
-                <Grid xs={6} md={4}>
-                    <ItemLeftAlign >
-                    <img src=" /src/images/Avatare/ja_ob.png" width="400rem" alt="Avatar1" />
-                    </ItemLeftAlign>
-                </Grid>
-                <Grid xs={6} md={8} mdOffset="auto" >
-                    <ItemLeftAlign>
-                        <h3 class="font-semibold">ja_ob</h3>
-                    </ItemLeftAlign>
-                    <ItemLeftAlign>
-                        <p>... will say 'hi' in a bit ...</p>
-                    </ItemLeftAlign>
-                </Grid>
-                <Grid xs={6} md={4} display="flex" justifyContent="end" alignItems="end">
-                    <ItemRightAlign>
-                    <img src="/src/images/Avatare/jO3_.png" width="400rem" alt="Avatar2" />
-                    </ItemRightAlign>
-                </Grid>
-                <Grid xs={6} md={8}>
-                    <ItemRightAlign>
-                        <h3 class="font-semibold">jO3</h3>
-                    </ItemRightAlign>
-                    <ItemRightAlign>
-                        <p>Specialist for backend and especially security related issues. From architecture, implementation, project management and communication with first users - j03  creates the necessary skeleton and outlines for the project.</p>
-                    </ItemRightAlign>
-                </Grid>
-                <Grid xs={6} md={4} display="flex" justifyContent="start" alignItems="start">
-                    <ItemLeftAlign>
-                    <img src="https://i.postimg.cc/x8bxFS7F/Avatar4.png"  alt="Avatar3" width="200rem"/>
-                    </ItemLeftAlign>
-                </Grid>
-                <Grid xs={6} md={8} mdOffset="auto">
-                    <ItemLeftAlign>
-                        <h3 class="font-semibold">sam_</h3>
-                        <p class="leading-relaxed">Enthusiast for technology and society - political scientist, sociology graduate and painter - came to programming through an unstoppable curiosity.</p>
-                    </ItemLeftAlign>
-                </Grid>
-
-                <Grid xs={6} md={4} display="flex" justifyContent="end" alignItems="end">
-                    <ItemRightAlign>
-                        <img src="https://i.postimg.cc/x8bxFS7F/Avatar4.png" alt="Avatar4" width="200rem" />
-                    </ItemRightAlign>
-                </Grid>
-
-                <Grid xs={6} md={8}>
-                    <ItemRightAlign>
-                        <h3 class="font-semibold">winzlieb</h3>
-                    </ItemRightAlign>
-                    <ItemRightAlign>
-                        <p>... will say 'hi' in a bit ...</p>
-                    </ItemRightAlign>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid xs={6} md={4}>
+                <Container>
+                    <img src=" /src/images/Avatare/ja_ob.png" width="350rem" alt="Avatar1" />
+                </Container>
             </Grid>
-        </Box>
+            <Grid xs={4}>
+                <Container>
+                    <h3 class="font-semibold">ja_ob</h3>
+                </Container>
+                <Container>
+                    <p class="leading-relaxed">... will say 'hi' in a bit ...</p>
+                </Container>
+            </Grid>
+
+            <Grid xs={6} md={8} mdOffset="auto" >
+            </Grid>
+
+
+            <Grid xs={6} md={4}>
+                <ItemRightAlign>
+                    <img src="/src/images/Avatare/jO3_.png" width="400rem" alt="Avatar2" />
+                </ItemRightAlign>
+                <ItemRightAlign>
+                    <h3 class="font-semibold">jO3</h3>
+                </ItemRightAlign>
+                <ItemRightAlign>
+                    <p class="leading-relaxed">Specialist for backend and especially security related issues. From architecture, implementation, project management and communication with first users - j03  creates the necessary skeleton and outlines for the project.</p>
+                </ItemRightAlign>
+            </Grid>
+
+
+            <Grid xs={6} md={8}>
+            </Grid>
+
+
+            <Grid xs={6} md={4} >
+                <ItemLeftAlign>
+                    <img src="https://i.postimg.cc/x8bxFS7F/Avatar4.png" alt="Avatar3" width="200rem" />
+                </ItemLeftAlign>
+                <ItemLeftAlign>
+                    <h3 class="font-semibold">sam_</h3>
+                    <p class="leading-relaxed">Enthusiast for technology and society - political scientist, sociology graduate and painter - came to programming via the fascination how tech shapes reality.</p>
+                </ItemLeftAlign>
+            </Grid>
+
+            <Grid xs={6} md={8} mdOffset="auto">
+            </Grid>
+
+            <Grid xs={6} md={4} display="flex" justifyContent="end" alignItems="end">
+                <ItemRightAlign>
+                    <img src="/src/images/Avatare/seb_pic.png" alt="Avatar4" width="400rem" />
+                </ItemRightAlign>
+            </Grid>
+
+            <Grid xs={6} md={8}>
+                <ItemRightAlign>
+                    <h3 class="font-semibold">winzlieb</h3>
+                </ItemRightAlign>
+                <ItemRightAlign>
+                    <p>... will say 'hi' in a bit ...</p>
+                </ItemRightAlign>
+            </Grid>
+        </Grid>
     );
 }
