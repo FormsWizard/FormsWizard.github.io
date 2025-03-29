2e0a8ed0156b4b814db26d969d7f09840269ac7a
Winzlieb <winzlieb@c3d2.de>
2025-03-29 10:02:40 +0100

    updateing lock files

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build https://formswizard.github.io/";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs, ... }:
